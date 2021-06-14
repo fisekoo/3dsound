ad5920300b345201825bec4ad1f64006391e9e21
payne <82342866+sonerpayne@users.noreply.github.com>
2021-06-14 16:30:38 +0300

    Update soundmanager.cs

--- soundmanager.cs
+++ soundmanager.cs
@@ -1,40 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class soundmanager : MonoBehaviour
 {
-    public AudioClip takeItem, Jumpscare, Thunder, Fire, putWood, putMouse, fire2, walk;
-    public AudioSource source;
-    public AudioSource walking;
-    public AudioSource carNoise;
-    karakter karakter;
-    void Start()
+    [SerializeField]
+    private AudioSource carNoise;
+
+    [SerializeField]
+    private Transform character;
+    // Don't forget to assign both variables on inspector.
+    
+    // if you want sound to come from a special place, use this variable instead of "this.transform"
+    [SerializeField]
+    private Transform particularAreaPos;
+    
+    public float maxDistance = 5f;
+    // You can change max distance if you want.
+    
+    private void Start()
     {
-        karakter = GameObject.FindGameObjectWithTag("Player").GetComponent<karakter>();
-        source = GetComponent<AudioSource>();
+        // Find character with tag "Player"
+        character = GameObject.FindGameObjectWithTag("Player").GetComponent<karakter>();
     }
-    private void Update()
+    
+    // You can use Update or FixedUpdate too. But in my case, i used LateUpdate
+    private void LateUpdate()
     {
-        sound3D(7, karakter.transform, this.transform, carNoise);
-        walkSound();
+        /* this script attached to the audio source itself. That's why i used "this.transform"
+        if you want sound to come from a special place, use particularAreaPos instead of "this.transform"*/
+        sound3D(maxDistance, character.transform, this.transform, carNoise);
     }
 
-    // Update is called once per frame
-    public void walkSound()
-    {
-        if (karakter.input != 0)
-        {
-            walking.loop = true;
-            if (!walking.isPlaying) walking.Play();
-        }
-        else walking.Stop();
-    }
+
+    // Method for 3D sound effect.
     public void sound3D(float maxDistance, Transform characterPos, Transform sourcePos, AudioSource audio)
     {
+        // Distance between character and audioSource.
         float distance = Vector2.Distance(characterPos.position, sourcePos.position);
+        
+        // for normalizing distance.
         float temp = distance / maxDistance;
+        
+        // if we didn't substract 1, volume would be max(1) at maxDistamce.
         float volume = 1 - temp;
+        
+        // Setting the volume.
         audio.volume = volume;
 
         // LOG INFORMATION //
@@ -43,3 +52,9 @@ public class soundmanager : MonoBehaviour
         Debug.Log("volume (1 - distance / maxDistance) " + volume);
     }
 }
+
+// SOURCE //
+/*
+Reddit thread: https://www.reddit.com/r/Unity2D/comments/43swwk/quick_3d_audio_question_for_2d_games/
+Thanks to Proximal_Pyro (https://www.reddit.com/user/Proximal_Pyro/)
+*/
